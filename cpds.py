9c20dc32ebfec506cb3ef33e5202ab3089630d54
Brian Lam <iBriaan@gmail.com>
2016-02-14 03:35:16 -0600

    Send requests to moss and scrape results

---
+++ cpds.py
@@ -0,0 +1,100 @@
+import subprocess
+from pprint import pprint
+import json
+import os
+import re
+import sys
+from lxml import html
+import requests
+
+## Grab a URL from a moss call
+## http://stackoverflow.com/questions/6883049/regex-to-find-urls-in-string-in-python
+def get_url(response):
+  urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', response)
+  return urls[0]
+
+## Get all percentages from a moss URL
+def get_percentages(url):
+  page = requests.get(url)
+  content = page.content
+  percentages = re.findall('[0-9]+%', content)
+  return percentages
+
+## Scrape a moss URL to find the highest percentage match
+def get_high_percentages(url, cutoff):
+  high_score = False
+  percentages = get_percentages(url)
+  if (len(percentages) > 0):
+    for score in percentages:
+      score = int(score.strip("%"))
+      if (score > cutoff):
+        high_score = True
+  return high_score
+
+## Send moss request
+def moss_compare(new_files, old_files):
+  cutoff = 90
+  write_filename = sys.argv[1] + "_comp_" + sys.argv[2] + ".txt"
+  print(write_filename)
+
+  for _new in new_files:
+    for _old in old_files:
+      if (get_extension(_new) == get_extension(_old) and _new != _old):
+        response = subprocess.check_output(["moss/moss", _new, _old])
+        url = get_url(response)
+        high_score = get_high_percentages(url, cutoff)
+        if (high_score):
+          percentages = get_percentages(url)
+          print "****************"
+          print "***** ALERT ****"
+          print "****************"
+          print _new
+          print _old
+          for _score in percentages:
+            print _score
+          print url
+          with open(write_filename, 'a') as file:
+            file.write("******" + "\n")
+            file.write(_new + "\n")
+            file.write(_old +"\n")
+            file.write(url + "\n")
+            for _score in percentages:
+              file.write(str(_score) + "\n")
+        else:
+          print("Okay: " + url)
+
+## Grab file extension
+def get_extension(file_path):
+  extension = os.path.splitext(file_path)[1]
+  return extension
+
+
+## Return files with matched name
+def walk(substring = ".php"):
+  matched_files = []
+  _dir = os.getcwd() + "/repositories"
+  for root, dirs, files in os.walk(_dir):
+    for file in files:
+      path = os.path.join(root, file)
+      if substring in path:
+        if not ".git" in path:
+          matched_files.append(path)
+  return matched_files  
+
+## Usage 
+def printUsage():
+  print "USAGE:"
+  print "python file-walker.py [new modules] [old modules]"
+  print "  Grab files whose full path contains the given substring, such as 'spring2015-module1'"
+
+
+## MAIN
+if __name__ == "__main__":
+  if (len(sys.argv) > 2):
+    old_files = walk(sys.argv[1])
+    print("Grabbed old files")
+    new_files = walk(sys.argv[2])
+    print ("Grabbed new files")
+    moss_compare(new_files, old_files)
+  else:
+    printUsage()
--- file-walker.py
+++ file-walker.py
@@ -1,13 +1,22 @@
 import os
 import sys
 
-def walk(extension = ".php"):
+def walk(substring = ".php"):
   _dir = os.getcwd() + "/repositories"
   for root, dirs, files in os.walk(_dir):
       for file in files:
-          if file.endswith(extension):
-               print(os.path.join(root, file))
+          path = os.path.join(root, file)
+          if substring in path:
+              if not ".git" in path:
+               print(path)
+
+def printUsage():
+  print "USAGE:"
+  print "python file-walker.py [substring]"
+  print "  Grab files whose full path contains the given substring, such as 'spring2015-module1'"
 
 if __name__ == "__main__":
   if (len(sys.argv) > 1):
     walk(sys.argv[1])
+  else:
+    printUsage()
